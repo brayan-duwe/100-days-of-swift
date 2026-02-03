c86cc2e395cb152c18a025adaa08d3c19b871971
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-02-03 19:40:31 -0300

    Day 1 - Simple types

---
+++ 100-days-of-code.playground/Pages/Day 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+// VARIABLES
+
+var str = "Hello, playground"
+str = "Goodbye, playground!"
+
+
+// INTEGERS
+
+var age = 21
+var population = 8_000_000
+
+
+// MULTI-LINE STRINGS
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// DOUBLES AND BOOLEANS
+
+let pi = 3.14159
+var awesome = true
+
+
+// STRING INTERPOLATION
+
+var greeting = "I'm \(age) years old."
+
+
+// CONSTANTS
+
+let value = 10
+
+
+// TYPE ANNOTATIONS
+
+let year: Int = 2026
+let height: Double = 1.73
+
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# 100 Days of Swift 🦅
+
+This repository documents my learning journey in Swift through the *100 days of Swift* course by Paul Hudson.
+
+Course: https://www.hackingwithswift.com/100
+
+
+
+| Day | Topics |
+| ----- | -------- |
+| [Day 1](/100-days-of-code.playground/Pages/Day%201.xcplaygroundpage/) | **Simple Types:** Variables, Constants, String, Int, Double, Bool, Interpolation, Type annotations |
