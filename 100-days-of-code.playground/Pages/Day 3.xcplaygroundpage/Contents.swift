089c786fc8de7c2ee87748dfdbc2f3f2d9a62a54
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-02-04 19:31:22 -0300

    Day 3 - Operators and conditions

--- 100-days-of-code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
+++ 100-days-of-code.playground/Pages/Day 3.xcplaygroundpage/Contents.swift
@@ -1,7 +1,114 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+// ARITHMETIC OPERATORS
 
-var greeting = "Hello, playground"
+let firstScore = 12
+let secondScore = 4
 
-//: [Next](@next)
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+let remainder  = 13 % secondScore
+
+
+// OPERATOR OVERLOADING
+
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
+
+
+// COMPOUND ASSIGNMENT OPERATORS
+
+var score = 10
+score += 5
+
+var quote = "The quick brown fox jumps over the lazy "
+quote += "dog."
+
+
+// COMPARASION OPERATORS
+
+let thirdScore = 6
+let fourthScore = 4
+
+thirdScore == fourthScore
+thirdScore != fourthScore
+
+thirdScore < fourthScore
+thirdScore >= fourthScore
+
+"Brayan" <= "Duwe"
+
+
+// CONDITIONS
+
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2 {
+    print("Aces – lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+// COMBINING CONDITIONS
+
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}
+
+
+// TERNARY OPERATORS
+
+let thirdCard = 11
+let fourthCard = 10
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+
+// SWITCH STATEMENTS
+
+let weather = "sunny"
+
+switch weather {
+case "sunny":
+    print("Wear sunscreen")
+case "rain":
+    print("Grab your umbrella")
+case "snow":
+    print("Wrap up warm")
+default:
+    print("Enjoy the day!")
+}
+
+
+// RANGE OPERATORS
+
+let newScore = 85
+
+switch newScore {
+case 0..<50: // ..< excludes the final value, if you want to include, use ...
+    print("You failed badly.")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
--- README.md
+++ README.md
@@ -8,8 +8,9 @@ Course: https://www.hackingwithswift.com/100
 
 | Day | Topics |
 | ----- | -------- |
-| [Day 1](/100-days-of-code.playground/Pages/Day%201.xcplaygroundpage/Contents.swift) | **Simple Types:** Variables, Constants, String, Int, Double, Bool, Interpolation, Type annotations |
-| [Day 2](/100-days-of-code.playground/Pages/Day%202.xcplaygroundpage/Contents.swift) | **Complex Types:** Arrays, Tuples, Sets, Dictionaries, Default values, Empty collections, Enum, Raw values |
+| [01](/100-days-of-code.playground/Pages/Day%201.xcplaygroundpage/Contents.swift) | **Simple Types:** Variables, Constants, String, Int, Double, Bool, Interpolation, Type annotations |
+| [02](/100-days-of-code.playground/Pages/Day%202.xcplaygroundpage/Contents.swift) | **Complex Types:** Arrays, Tuples, Sets, Dictionaries, Default values, Empty collections, Enum, Raw values |
+| [03](/100-days-of-code.playground/Pages/Day%203.xcplaygroundpage/Contents.swift) | **Operators and conditions:** Arithmetic operators, Operator overloading, Compound assignment operators, Conditions, Ternary operator, Switch statements, Range operators |
 
 
 
