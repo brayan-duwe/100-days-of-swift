2fb373a44d33cb0aa2fc9d00fe49f8609c3ac427
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-02-21 23:59:23 -0300

    Challenge 4

---
+++ 100-days-of-code.playground/Pages/Checkpoint 4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+enum squareRootError: Error {
+    case numberHigherOrLower
+    case noRoot
+    
+}
+
+func squareRoot(_ number: Int) throws -> Int {
+    if number < 1 || number > 10000 {
+        throw squareRootError.numberHigherOrLower
+    }
+    for var root in 1..<number {
+        root * root
+        if root * root == number {
+            return root
+        }
+    }
+    throw squareRootError.noRoot
+}
+    
+try squareRoot(16)
